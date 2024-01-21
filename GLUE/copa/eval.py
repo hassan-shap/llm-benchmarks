d0ab57009210d46943c1c485f8b77dea49e37279
hassan-shap <hassan.shapp@gmail.com>
2024-01-21 03:42:54 +0000

    initial commit

---
+++ GLUE/copa/eval.py
@@ -0,0 +1,195 @@
+
+import torch
+from datasets import load_dataset
+from transformers import (
+    AutoModelForCausalLM,
+    AutoTokenizer,
+    BitsAndBytesConfig,
+    logging,
+)
+
+import time 
+import json
+
+from datasets.utils.logging import disable_progress_bar, enable_progress_bar
+
+# ## Load model
+# 
+# model_name = "/data/opt-350m"
+# model_name = "mistralai/Mistral-7B-Instruct-v0.2"
+# model_name = "/data/llama-hf/Llama-2-7b-hf"
+# model_name = "mistralai/Mistral-7B-v0.1"
+model_name = "meta-llama/Llama-2-7b-hf"
+
+cutoff_len = 4096
+torch.cuda.empty_cache()
+
+bnb_config = BitsAndBytesConfig(
+    load_in_4bit=True,
+    bnb_4bit_use_double_quant=True,
+    bnb_4bit_quant_type="nf4",
+    bnb_4bit_compute_dtype=torch.bfloat16,
+)
+device_map = {"": 0}
+# Load model
+# base_model = AutoModelForCausalLM.from_pretrained(
+model = AutoModelForCausalLM.from_pretrained(
+    model_name,
+    quantization_config=bnb_config,
+    device_map=device_map,
+    # cache_dir = "/dev/shm/hassan/.cahce/"
+)
+
+# depth = len(base_model.base_model.layers)
+# num_removed_layers = 10
+# first_layer_dropped = depth - num_removed_layers -1
+# layers_dropped = list(range(first_layer_dropped, depth-1))
+# print(f"We are removing: {layers_dropped} layers")
+# base_model.base_model.layers = torch.nn.ModuleList([block for idx, block in enumerate(base_model.base_model.layers) if idx not in layers_dropped])
+# model = base_model
+# del base_model
+
+model.eval()
+
+# model.config.use_cache = False
+# model.config.pretraining_tp = 1
+
+# Load tokenizer
+tokenizer = AutoTokenizer.from_pretrained(model_name, trust_remote_code=True)
+if "Llama-2" in model_name or "Mistral" in model_name:
+    print("Doing Llama tokenizer thingy")
+    # tokenizer.pad_token_id = tokenizer.bos_token_id
+    tokenizer.pad_token = tokenizer.bos_token
+
+tokenizer.padding_side = "right"
+
+bos = tokenizer.bos_token_id
+eos = tokenizer.eos_token_id
+pad = tokenizer.pad_token_id
+print("pre-trained model's BOS EOS and PAD token id:",bos,eos,pad," => It should be 1 2 None")
+
+s0 = "1" #"choice1"
+s1 = "2" #"choice2"
+TF_idx = [
+    tokenizer(s0, add_special_tokens=False).input_ids[0],
+    tokenizer(s1, add_special_tokens=False).input_ids[0],
+ ]
+
+TF_idx_conn = [
+    tokenizer("_"+s0, add_special_tokens=False).input_ids[-1],
+    tokenizer("_"+s1, add_special_tokens=False).input_ids[-1],
+ ]
+TF_idx_double = TF_idx + TF_idx_conn
+idx_TF = [s0, s1]
+print(TF_idx_double)
+choice_idx = {s0 :0, s1:1}
+
+n_shot_vals = [2]#range(6)
+
+disable_progress_bar()
+
+outfname  = "boolq-" + model_name.split('/')[1] + '.json'
+out_json = "results/" + outfname
+
+with open(out_json, 'w') as json_file:
+
+    for i_n, n_shot in enumerate(n_shot_vals):
+        tic = time.time()
+        # if n_shot == 0:
+        #     batch_size = 40
+        if n_shot <= 1:
+            batch_size = 64
+        elif n_shot == 2:
+            batch_size = 32
+        elif n_shot == 3:
+            batch_size = 16
+        elif n_shot == 4:
+            batch_size = 8
+        else:
+            batch_size = 4
+
+        # dataset = load_dataset("json", data_files={
+        #         # 'test' : f"boolq-data/{n_shot}_shot_examples.json"
+        #         'test' : f"boolq-data/{n_shot}_shot_examples_small.json"
+        #     })
+
+        dataset = load_dataset("hassansh/copa_n_shot", name = f"{n_shot}_shot")
+        dataset = dataset['test'].select([1,25,210])
+        num_batches = dataset.num_rows// batch_size
+
+        acc = 0
+        acc_TF = 0
+        acc_TF_list = []
+        for i_b in range(num_batches+1):
+            # print(i_b)
+            if i_b< num_batches:
+                ex1 = dataset[i_b*batch_size:(i_b+1)*batch_size]['input']
+                answers = dataset[i_b*batch_size:(i_b+1)*batch_size]['target']
+            else:
+                if dataset.num_rows % batch_size > 0:
+                    # print(len(mmlu_dataset['input']),i_b*batch_size)
+                    ex1 = dataset[i_b*batch_size:]['input']
+                    answers = dataset[i_b*batch_size:]['target']
+                else:
+                    break
+
+            # input_ids = tokenizer(ex1, padding='max_length', return_tensors='pt', max_length=cutoff_len).input_ids
+            input_ids = tokenizer(ex1, padding=True, return_tensors='pt').input_ids
+            input_ids = input_ids.to(device=0)
+
+            with torch.no_grad():
+                output = model(input_ids)
+                answers_2 = output.logits.squeeze()
+                if len(ex1) == 1:
+                    answers_2 = answers_2.unsqueeze(0)
+
+
+            ones = torch.ones_like(input_ids)
+            last_token = input_ids == ones
+            row_indices = torch.arange(input_ids.size(0))
+            last_token = (torch.sum(last_token, dim = 1) + 1) * (-1)
+            if "Llama-2" in model_name or "Mistral" in model_name:
+                last_token+=1
+
+            top_choices_TF = torch.argmax(answers_2[row_indices,last_token,:][:,TF_idx_double],dim=-1) % 2
+            print(answers_2[row_indices,last_token,:][:,TF_idx_double])
+            # print(top_choices_TF)
+            # print(answers)
+            # final_prediction = [ tokenizer.decode(TF_idx_double[i])[-1] for i in top_choices_TF]
+            acc_TF += torch.sum(top_choices_TF.cpu() == torch.tensor(answers)).item()
+
+            # top_choices = torch.argmax(answers_2[row_indices,last_token,:], dim= -1)
+            # print(top_choices)
+            # print(tokenizer.decode(top_choices))
+            # print(len(answers))
+            # top_choices_txt = tokenizer.decode(top_choices).split()
+            # print(top_choices_txt)
+            # print(len(top_choices_txt))
+            for i_a, answer in enumerate(answers):
+                # print(i_r, answers[i_r])
+                response = tokenizer.decode(torch.argmax(answers_2[i_a,last_token[i_a],:], dim = -1))
+                # print(f"({idx_TF[answer]}, {response}): {response == idx_TF[answer]}")
+                # print(f"({answer}, {top_choices_TF[i_a]}): {top_choices_TF[i_a] == answer}")
+                # print(f"{''.join(['_']*20)}")
+                print(response, idx_TF[answer], idx_TF[answer]== response)
+                acc += (response == idx_TF[answer])
+            acc_TF_list += (top_choices_TF.cpu() == torch.tensor(answers)).tolist()
+        # acc /= dataset.num_rows
+        # acc_TF /= dataset.num_rows
+        print(n_shot, dataset.num_rows, acc, acc_TF)
+        # print(acc_TF_list)
+        toc = time.time()
+
+        print("Elapsed time: ", toc-tic, " sec")
+
+        eval_data = {'n_shot' : n_shot,
+                'accuracy' : acc, 
+                'accuracy_TF' : acc_TF, 
+                # 'cross_entropy' : cross_entropy, 
+                'time' : toc-tic}
+
+        json_file.write(json.dumps(eval_data) + '\n')
+
+# dataset = load_dataset("json", data_files={
+#              'test': out_json} )
+# dataset.push_to_hub("hassansh/boolq-"+ model_name.split('/')[1])
---
+++ GLUE/eval_andrey.py
@@ -0,0 +1,272 @@
+from transformers import AutoModelForCausalLM, AutoTokenizer
+from datasets import load_dataset, load_metric
+import torch
+import sys
+from torch.utils.data import Subset
+import random
+import os 
+import torch.nn as nn
+from datetime import date
+import json 
+import shutil
+from torch.nn.functional import softmax
+from torch.utils.data import DataLoader
+from transformers import BitsAndBytesConfig
+from peft import (
+    LoraConfig,
+    get_peft_model,
+    set_peft_model_state_dict
+)
+from peft import prepare_model_for_kbit_training
+from transformers import BitsAndBytesConfig, AutoModelForCausalLM, AutoTokenizer, LlamaTokenizer
+from peft import PeftModel
+
+device = "cuda:0"
+device_map = "cuda:0"
+instruction = None
+BATCH_SIZE = 4
+top_k = 10
+        
+
+def evaluate_model(model = None,
+                   tokenizer = None,
+                   dataset_name : str = '',
+                   split : str = '', 
+                   num_shots : int = 0, 
+                   save : bool = False, 
+                   out_path : str = '', 
+                   details : bool = False):
+
+
+
+    # LLama2 does not have a padding token so I pad with BoS
+
+    tokenizer.pad_token_id = 1
+
+    # Load the dataset
+    dataset = load_dataset(dataset_name, name = f"{num_shots}_shot")[split]
+
+    # Create the n-shot string to append
+    # if num_shots > 0:
+    #     dataset_size = len(dataset)
+    #     random_indices = random.sample(range(dataset_size), num_shots)
+    #     random_subset = Subset(dataset, random_indices)
+
+    #     for i in range(num_shots)
+
+    # Prepare variables to collect MMLU data
+    answers = []
+    top_tokens = []
+    top_tokens_decoded = []
+    top_probs = []
+    tf_probs = []
+    acc_per_question = []
+    confidence = []
+    acc = 0
+
+    num_batches = dataset.num_rows// BATCH_SIZE
+
+    # Evaluation loop
+    for i_b in range(num_batches+1):
+        # print(i_b)
+        if i_b< num_batches:
+            batch = dataset[i_b*BATCH_SIZE:(i_b+1)*BATCH_SIZE]
+        else:
+            if dataset.num_rows % BATCH_SIZE > 0:
+                batch = dataset[i_b*BATCH_SIZE:]
+            else:
+                break
+
+        #input_ids = tokenizer(prompt_text, return_tensors='pt').input_ids
+        input_ids = tokenizer(batch['input'], padding=True, return_tensors='pt').input_ids
+        input_ids = input_ids.to(device=device)
+        #print("DECODED INPUT:", repr(tokenizer.decode(input_ids.tolist()[0], skip_special_tokens=False)))
+        #print(input_ids, input_ids.shape)
+
+        ### Getting the position of the last non-padding token
+        ones = torch.ones_like(input_ids)
+        last_token = input_ids == ones
+        row_indices = torch.arange(input_ids.size(0))
+        last_token = (torch.sum(last_token, dim = 1) + 1) * (-1)
+
+        # Extra 1 from BoS and the beginning of each string.
+        if tokenizer.bos_token_id == 1:
+            last_token+=1
+
+        # print(row_indices, last_token)
+        # print("Last token", input_ids[row_indices, last_token])
+
+        # Generate the model's response
+        with torch.no_grad():
+            output = model(input_ids)
+            outs = output.logits.squeeze()
+            if len(batch['input']) == 1:
+                outs = outs.unsqueeze(0)
+
+        #print(f"Model output shape: {outs.shape}")
+
+        #print(f"Model output probs on last non-padding token: {softmax(outs[row_indices,last_token,:],dim=1).shape}")
+
+        probabils, out_tokens = torch.topk(softmax(outs[row_indices,last_token,:],dim=1), top_k)
+        out_tokens = out_tokens.detach().clone().cpu().tolist()
+        probabils = probabils.detach().clone().cpu().tolist()
+        top_tokens += out_tokens
+        top_probs += probabils
+
+        top_tokens_dec = tokenizer.batch_decode(out_tokens, skip_special_tokens=False)
+
+        top_tokens_decoded += top_tokens_dec
+        #print(f"Highest probability tokens are: {top_tokens_dec}, with probabilities: {probabils}")
+
+        TF_idx = [
+            tokenizer(s0, add_special_tokens=False).input_ids[0],
+            tokenizer(s1, add_special_tokens=False).input_ids[0],
+        ]
+        TF_idx_conn = [
+            tokenizer("_"+s0, add_special_tokens=False).input_ids[-1],
+            tokenizer("_"+s1, add_special_tokens=False).input_ids[-1],
+        ]
+        TF_ids_list = TF_idx + TF_idx_conn
+        
+
+        # print(tokenizer.decode(adcd_ids_list, skip_special_tokens=False))
+        # print(last_token, "\n", input_ids)
+        probs = softmax(outs[row_indices,last_token,:], dim=1)
+
+        tf_predictions = probs[:,TF_ids_list]
+
+        # normalize abcd_probs for saving later
+        #abcd_predictions = abcd_predictions / abcd_predictions.sum(1).unsqueeze(1)
+        tf_probs += tf_predictions.detach().clone().cpu().tolist()
+        #print(f"Probabilities: {abcd_predictions} and the sum to 1: {abcd_predictions.sum(1)}")
+
+        # for now dumb measure of confidence
+        top_tf_probs, _ = torch.topk(tf_predictions, 2)
+        confidence_per_question = torch.abs(top_tf_probs[:,0] - top_tf_probs[:,1])
+        confidence += confidence_per_question.detach().clone().cpu().tolist()
+        #print(f"shape of top_abcd_probs, {top_abcd_probs}", top_abcd_probs.shape)
+        final_prediction = torch.argmax(outs[row_indices,last_token,:][:,TF_ids_list], dim=1) % 2
+        # print(f"Final prediction {final_prediction.data} vs Correct answer: {batch['target']}")
+        answers += final_prediction.tolist()
+        preds = final_prediction == torch.tensor(batch['target']).to(device=device)
+        acc_per_question += preds.tolist()
+        acc += torch.sum(preds)
+        # print(f"Number of correct answers {acc}")
+    #print(f"Prediction array {answers}, number of predictions {len(answers)}, Totatl number of correct answers {acc}")
+    #print(f"Top tokens: {top_tokens} with probs: {top_probs}. The ABCD probs are: {abcd_probs}")
+    acc = acc.item() /  len(answers)
+    # convert lists to a single dict
+
+    if details:
+        eval_data = {'model' : model_name,
+                    'accuracy' : acc, 
+                    'accuracy_per_question' : acc_per_question,
+                    'model_answers' : answers,
+                    'tf_probs' : tf_probs,
+                    'most_probable_tokens' : top_tokens,
+                    'most_probable_tokens_decoded' : top_tokens_decoded,
+                    'most_probable_tokens_probs' : top_probs}
+    else:
+        eval_data = {'model' : model_name,
+                    'accuracy' : acc}
+
+    # for key in eval_data.keys():
+    #     print(f'\n\n====================={key}:=======================\n {eval_data[key]}')
+
+    if save:
+        # make directory where to save
+        out =  dataset_name.split("/")[-1] + f"_{num_shots}"
+        # add day stamp
+        today = date.today()
+        out = out 
+        out_dir = os.path.join(out_path, out)
+        # delete if dir already exists
+        if os.path.exists(out_dir):
+            shutil.rmtree(out_dir)
+        os.mkdir(out_dir)
+        # actiual saving of the logs
+        for key in eval_data.keys():
+            T = eval_data[key]
+            log_path = os.path.join(out_dir, f"{key}.json")
+            print(f"saving {key} to", log_path)
+            with open(log_path, 'w') as f:
+                json.dump(T, f)
+    return acc
+
+
+# Example usage
+model_name = "meta-llama/Llama-2-7b-hf"  # Replace with your model's name
+#model_name = "mistralai/Mistral-7B-v0.1"
+#model_name = "facebook/opt-125m" 
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+print(f"EOS {tokenizer.eos_token_id} BOS {tokenizer.bos_token_id} PAD {tokenizer.pad_token_id}")
+############################################################
+################### only changing these ####################
+#### boolq ########
+# dataset_name = "hassansh/boolq_n_shot"  # The boolq dataset name in Hugging Face Datasets
+# s0 = "No" #"False"
+# s1 = "Yes" #"True"
+# #### copa ########
+# dataset_name = "hassansh/copa_n_shot"  # The copa dataset name in Hugging Face Datasets
+# s0 = "1" #"choice1"
+# s1 = "2" #"choice2"
+# #### rte ########
+dataset_name = "hassansh/rte_n_shot"  # The rte dataset name in Hugging Face Datasets
+s0 = "False" #"not_entailment"
+s1 = "True" #"entailment"
+# ############################################################
+split = "test"  # Specify the dataset split (e.g., 'test')
+save = True
+out_path = 'gromovand/'
+details = True
+try:
+    os.makedirs(out_path)
+    print(f"Directory '{out_path}' created successfully")
+except FileExistsError:
+    print(f"Directory '{out_path}' already exists")
+
+#### Load the pretrained model and tokenizer ####
+
+#16 bit
+# model = AutoModelForCausalLM.from_pretrained(model_name)
+# model = model.to(device = device)
+
+# 4bit
+bnb_config = BitsAndBytesConfig(
+            load_in_4bit=True,
+            bnb_4bit_use_double_quant=True,
+            bnb_4bit_quant_type="nf4",
+            bnb_4bit_compute_dtype=torch.bfloat16,
+        )
+model = AutoModelForCausalLM.from_pretrained(
+            model_name, 
+            quantization_config=bnb_config,
+            device_map=device_map
+        )
+# # #PEFT LORA LOAD
+# MODEL_PATH = "/fsx-scaling/ktirumala/andrey_pruning_runs/layer_drop_platypus_13b/dec_25_lora_r_64_layer_start_drop_35"
+
+# blocks_to_remove = [35, 36, 37, 38]
+# print(f"We are removing: {blocks_to_remove} layers")
+# base_model.base_model.layers = nn.ModuleList([block for idx, block in enumerate(base_model.base_model.layers) if idx not in blocks_to_remove])
+
+# model = PeftModel.from_pretrained(
+#         base_model,
+#         MODEL_PATH,
+#         torch_dtype=torch.bfloat16,
+#     )
+# del base_model
+
+model.eval()
+
+for n_shot in range(6):
+    score = evaluate_model(model,
+                        tokenizer,
+                        dataset_name = dataset_name, 
+                        split = split, 
+                        num_shots = n_shot,
+                        save = save,
+                        out_path = out_path,
+                        details = details)
+
+    print(f"Model performance on {n_shot} is {score}")
